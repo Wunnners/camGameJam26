eb0ae015b7f9dcf4a0d28522d9a9ca6e49d0f28d
Atharv-Nema <atharvnema12@gmail.com>
2026-02-28 13:44:52 +0000

    can write landscape as a string

--- config.py
+++ config.py
@@ -4,3 +4,17 @@ PLAYER_SPEED = 5
 WHITE = (255, 255, 255)
 WALL_COLOR = (70, 70, 80)
 BG_COLOR = (30, 30, 35)
+TILE_SIZE = 50
+LEVEL_MAP = [
+    "WWWWWWWWWWWWWWWWWWWW",
+    "W..................W",
+    "W...P..............W",
+    "W........WWWW......W",
+    "W...........W......W",
+    "W...........W......W",
+    "WWWWWW......W......W",
+    "W...........W......W",
+    "W....WWWWWWWW......W",
+    "W..................W",
+    "WWWWWWWWWWWWWWWWWWWW",
+]
